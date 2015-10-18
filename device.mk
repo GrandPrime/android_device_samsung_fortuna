31e2c2501aa873a3b9513119191948ca7ef3db80
baran0119 <baran0119@gmail.com>
2015-10-18 16:00:34 +0200

    fortuna: device.mk: add to overlay samsung qcom-common

--- device.mk
+++ device.mk
@@ -211,7 +211,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-	$(LOCAL_PATH)/overlay
+	$(LOCAL_PATH)/overlay \
+	device/samsung/qcom-common/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
